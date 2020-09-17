45b46b7307dec8c6b23ec4073154b8d6c055e3dc
John Bohannon <imjohnbo@github.com>
2020-09-16 20:28:21 -0400

    feat: produce artifact with csv

--- index.js
+++ index.js
@@ -1,61 +1,103 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const artifact = require('@actions/artifact');
+const owner = core.getInput('owner');
 const token = core.getInput('token');
+const artifactFile = `${owner}-actions.csv`;
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const octokit = github.getOctokit(token, {
-    log: console,
+  log: console
 });
 
+const getActionsFromOwnerRepos = async (owner) => {
+  let reposByOwner;
+  let actions = [];
+
+  core.info(`Retrieving actions by: ${owner}`);
+
+  for await (const response of octokit.paginate.iterator(octokit.repos.listForUser, { username: owner })) {
+    reposByOwner = response.data;
+    const actionsInPage = await Promise.all(reposByOwner.filter(r => r.size > 0).map(getUrlToAction));
+    actions = actions.concat(actionsInPage);
+  }
+
+  return actions;
+};
+
 const getUrlToAction = async (repository) => {
-    try {
-        let url = '';
-        const owner = repository.owner.login;
-        const repo = repository.name;
-        const path = 'action.yml';
+  try {
+    let url = '';
+    const owner = repository.owner.login;
+    const repo = repository.name;
+    const path = 'action.yml';
 
-        core.info(`Checking if ${owner}/${repo} is an action...`);
-    
-        const { data: action } = await octokit.repos.getContent({
-            owner,
-            repo,
-            path
-        });
-    
-        if (action && action.type === 'file' && action.size > 0) {
-            url = repository.html_url;
-        }
+    core.info(`Checking if ${owner}/${repo} is an action...`);
 
-        if (url) {
-            core.info(`${owner}/${repo} seems to be an action at url ${url}`);
-        }
-    
-        return url;
+    const { data: action } = await octokit.repos.getContent({
+      owner,
+      repo,
+      path
+    });
+
+    if (action && action.type === 'file' && action.size > 0) {
+      url = repository.html_url;
     }
-    catch (e) {
-        return '';
+
+    if (url) {
+      core.info(`${owner}/${repo} seems to be an action at url ${url}`);
     }
-}
 
-async function run() {
-    try {
-        const owner = core.getInput('owner');
-        let actions = [];
-        let reposByOwner;
+    return {
+      url: url
+    };
+  } catch (e) {
+    return '';
+  }
+};
 
-        core.info(`Retrieving actions by: ${owner}`);
+const createCsvFromActions = async (actions) => {
+  const csvWriter = createCsvWriter({
+    path: artifactFile,
+    header: [
+      { id: 'url', title: 'URL' }
+    ]
+  });
 
-        for await (const response of octokit.paginate.iterator(octokit.repos.listForUser, { username: owner })) {
-            reposByOwner = response.data; 
-            const actionsInPage = (await Promise.all(reposByOwner.filter(r => r.size > 0).map(getUrlToAction)))
-                .filter(a => a);
-            actions = actions.concat(actionsInPage);
-        }
-        const results = JSON.stringify(actions);
-        core.info(`Actions: ${results}`);
-        core.setOutput(results);
-        
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+  await csvWriter.writeRecords(actions);
+};
+
+const createArtifactFromCsv = async () => {
+  const artifactClient = artifact.create();
+  const artifactName = `${owner}-actions`;
+  const files = [
+    artifactFile
+  ];
+  const rootDirectory = '.';
+  const options = {
+    continueOnError: false
+  };
+
+  await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
+};
+
+const setOutput = (actions) => {
+  const results = JSON.stringify(actions);
+  core.info(`Actions: ${results}`);
+  core.setOutput('actions', results);
+};
+
+async function run () {
+  try {
+    const actions = await getActionsFromOwnerRepos(owner);
+
+    await createCsvFromActions(actions);
+
+    await createArtifactFromCsv();
+
+    setOutput();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
